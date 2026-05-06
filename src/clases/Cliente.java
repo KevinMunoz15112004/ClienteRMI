1f08888184d14b36878e6891ce074a3cd863a850
Kevin M <kevinmunoz57898@gmail.com>
2026-05-06 17:11:10 -0500

    commit

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -3,17 +3,17 @@ package clases;
 import java.rmi.Naming;
 
 public class Cliente {
-    public static void consultar(int id) throws Exception{
+    public static String consultar(int id) throws Exception{
         String resultado = null;
         String rmiObjectName = "rmi://localhost/Datos";
         Servidor servicio = (Servidor) Naming.lookup(rmiObjectName);
-        servicio.consultar(id);
-
+        String r = servicio.consultar(id);
+        return r;
     }
 
-    static void main(String[] args) throws Exception{
+    static void main(String[] args){
         try {
-            consultar(1);
+            System.out.println(consultar(1));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
